04751b64b625de91bfaf2fd1788e409143526d09
Us10n <68775862+Us10n@users.noreply.github.com>
2021-04-25 13:13:18 +0300

    Update CounterController.java

--- src/main/java/com/web/restservice/controllers/CounterController.java
+++ src/main/java/com/web/restservice/controllers/CounterController.java
@@ -1,17 +1,13 @@
 package com.web.restservice.controllers;
 
-import com.web.restservice.entities.RequestCounter;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import com.web.restservice.services.RequestCounter;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-public class CounterController{
-    @GetMapping("/getcounter")
-    public String outCounter(Model model){
-        model.addAttribute("counter", "Было выполнено  " + RequestCounter.INSTANCE.getCounter() +" запросов на главный контроллер");
-
-        return "counter";
+@RestController
+public class CounterController {
+    @GetMapping("/counter")
+    public Integer outCounter() {
+        return RequestCounter.INSTANCE.getCounter();
     }
-
 }
